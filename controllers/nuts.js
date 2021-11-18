20ee5a1dde0ce1f1bc56317eb4659d6be48ed2ae
sateesh6131 <89415533+sateesh6131@users.noreply.github.com>
2021-11-17 22:42:12 -0600

    Forms modified

--- controllers/nuts.js
+++ controllers/nuts.js
@@ -6,8 +6,15 @@ exports.nuts_list = function(req, res) {
 }; 
  
 // for a specific nuts. 
-exports.nuts_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: nuts detail: ' + req.params.id); 
+exports.nuts_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await nuts.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle nuts create on POST. 
@@ -15,14 +22,38 @@ exports.nuts_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: nuts create POST'); 
 }; 
  
-// Handle nuts delete form on DELETE. 
-exports.nuts_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: nuts delete DELETE ' + req.params.id); 
+// Handle nuts delete on DELETE. 
+exports.nuts_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await nuts.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
- 
+
 // Handle nuts update form on PUT. 
-exports.nuts_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: nuts update PUT' + req.params.id); 
+exports.nuts_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await nuts.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.nuts_type)  
+               toUpdate.nuts_type = req.body.nuts_type; 
+        if(req.body.Cost) toUpdate.Cost = req.body.Cost; 
+        if(req.body.Weight) toUpdate.Weight = req.body.Weight; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 // List of all nutss 
 exports.nuts_list = async function(req, res) { 
@@ -66,4 +97,59 @@ exports.nuts_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+}; 
+
+// Handle a show one view with id specified by query
+exports.nuts_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await nuts.findById( req.query.id)
+        res.render('nutsdetail', {
+            title: 'nuts Detail', 
+            toShow: result
+        });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a nuts.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.nuts_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('nutscreate', { title: 'nuts Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a nuts. 
+// query provides the id 
+exports.nuts_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await nuts.findById(req.query.id) 
+        res.render('nutsupdate', { title: 'nuts Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.nuts_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await nuts.findById(req.query.id) 
+        res.render('nutsdelete', { title: 'nuts Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/nuts.js
+++ routes/nuts.js
@@ -4,4 +4,15 @@ var router = express.Router();
  
 /* GET nutss */ 
 router.get('/', nuts_controlers.nuts_view_all_Page ); 
+
+/* GET detail nuts page */ 
+router.get('/detail', nuts_controlers.nuts_view_one_Page); 
+/* GET create nuts page */ 
+router.get('/create', nuts_controlers.nuts_create_Page); 
+/* GET create update page */ 
+router.get('/update', nuts_controlers.nuts_update_Page); 
+/* GET create nuts page */ 
+router.get('/delete', nuts_controlers.nuts_delete_Page); 
 module.exports = router; 
+ 
+ 
