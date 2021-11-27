7762687158affe35d31d7146fde3d20d3e64459a
sateesh6131 <89415533+sateesh6131@users.noreply.github.com>
2021-11-26 20:37:14 -0600

    added authentication to add endpoints

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
+var nuts = require("./models/nuts"); 
 
 const connectionString =  
 process.env.MONGO_CON 
@@ -17,8 +33,6 @@ var nutsRouter = require('./routes/nuts');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
-var nuts = require('./models/nuts');
-
 
 var app = express();
 
@@ -30,6 +44,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Get the default connection 
@@ -39,11 +60,11 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
 db.once("open", function(){ 
  console.log("Connection to DB succeeded")}); 
+
  // We can seed the collection if needed on server start 
 async function recreateDB(){ 
   // Delete everything 
   await nuts.deleteMany(); 
- 
   let instance1 = new 
 nuts({nuts_type:"Ground Nuts",  Weight:20, 
 cost:300}); 
@@ -53,23 +74,22 @@ cost:400});
 let instance3 = new 
 nuts({nuts_type:"badam",  Weight:25, 
 cost:200});
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("First object saved") 
-  }); 
-  instance2.save( function(err,doc) { 
-    if(err) return console.error(err); 
-    console.log("First object saved") 
-}); 
-instance3.save( function(err,doc) { 
-  if(err) return console.error(err); 
-  console.log("First object saved") 
-}); 
-} 
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+  instance2.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("Second object saved")
+  });
+  instance3.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("Third object saved")
+    });
+  } 
  
 let reseed = true; 
-if (reseed) { recreateDB();} 
- 
+if (reseed) { recreateDB();}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -78,6 +98,13 @@ app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/nuts.js
+++ routes/nuts.js
@@ -7,10 +7,21 @@ router.get('/', nuts_controlers.nuts_view_all_Page );
 
 /* GET detail nuts page */ 
 router.get('/detail', nuts_controlers.nuts_view_one_Page); 
+
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET create nuts page */ 
 router.get('/create', nuts_controlers.nuts_create_Page); 
 /* GET create update page */ 
-router.get('/update', nuts_controlers.nuts_update_Page); 
+router.get('/update',secured, nuts_controlers.nuts_update_Page); 
 /* GET create nuts page */ 
 router.get('/delete', nuts_controlers.nuts_delete_Page); 
 module.exports = router; 
